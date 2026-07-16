1a1d667b94d395b02aa6d6622d4a03b8780c4d7d
Kabir Alkasim <codesmartng1@gmail.com>
2026-07-16 08:15:44 +0100

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,1 +1,9 @@
 
+from app import db
+from flask_login import UserMixin
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    subscription_status = db.Column(db.String(50), default="inactive")
