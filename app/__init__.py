e79ce6da5f4f9d03f82c5a6aa836f9931997296d
Kabir Alkasim <codesmartng1@gmail.com>
2026-07-16 08:06:09 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,22 @@
 
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+login_manager = LoginManager()
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'your_secret_key'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///forex.db'
+
+    db.init_app(app)
+    login_manager.init_app(app)
+
+    from .routes import auth, robot, subscription
+    app.register_blueprint(auth.bp)
+    app.register_blueprint(robot.bp)
+    app.register_blueprint(subscription.bp)
+
+    return app
